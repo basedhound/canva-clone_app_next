6b7f883aa17310957312e1ea0e98491e0c6ae7df
Jozef S <franck.vukelic@gmail.com>
2024-04-14 21:54:41 +0700

    Update cancel URL in checkoutCredits function

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -32,7 +32,7 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     },
     mode: 'payment',
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
   })
 
   redirect(session.url!)
