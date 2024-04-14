eb5f14ecf96c327609365b1d45598c191a90e2d3
Jozef S <franck.vukelic@gmail.com>
2024-04-14 19:22:09 +0700

    Remove unnecessary public route

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
+  publicRoutes: ["/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
 export const config = {
